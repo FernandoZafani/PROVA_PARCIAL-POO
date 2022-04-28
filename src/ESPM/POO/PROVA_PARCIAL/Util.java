4543d13e6f79eb25863c3df7c29a981f7f2c84fd
Fernando Zafani <zafani07@gmail.com>
2022-04-27 21:26:00 -0300

    util

--- src/ESPM/POO/PROVA_PARCIAL/Util.java
+++ src/ESPM/POO/PROVA_PARCIAL/Util.java
@@ -1,91 +1,90 @@
 package ESPM.POO.PROVA_PARCIAL;
+
 import static javax.swing.JOptionPane.*;
 
 import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 
-
 public class Util {
-    static ArrayList<Reserva> reserva= new ArrayList<>();
+    static ArrayList<Reserva> reserva = new ArrayList<>();
 
     public static void reservar() {
         String nome, cpf, cnpj, tpPessoa, pagamento;
-        Cliente cliente=null;
+        Cliente cliente = null;
 
-        nome=JOptionPane.showInputDialog("Insira o nome:");
-        tpPessoa=JOptionPane.showInputDialog("Qual o tipo de cliente?[F|J]").toLowerCase();
-        if(tpPessoa.equals("f")){
-            cpf=showInputDialog("Insira seu CPF:");
-        
-            PessoaFisica pf=new PessoaFisica(nome,cpf);
-            cliente=pf;
-            pagamento=showInputDialog("Escolha o método de pagamento: \n"+
-                                        "V: pagamento à vista \n"+
-                                        "P: pagamento parcelado").toLowerCase();
-                
-                if(pagamento.equals("v")){
-                    Reserva reserva1= new Reserva(cliente, true);
-                    reserva.add(reserva1);
-                }else if(pagamento.equals("p")){
-                    Reserva reserva1= new Reserva(cliente, false);
-                    reserva.add(reserva1);
-                }else{
-                    showMessageDialog(null, "Resposta inválida");
-                }
-            
+        nome = JOptionPane.showInputDialog("Insira o nome:");
+        tpPessoa = JOptionPane.showInputDialog("Qual o tipo de cliente?[F|J]").toLowerCase();
+        if (tpPessoa.equals("f")) {
+            cpf = showInputDialog("Insira seu CPF:");
 
-        }else if(tpPessoa.equals("j")){
-            cnpj=showInputDialog("Insira seu CNPJ:");
-            PessoaJuridica pj= new PessoaJuridica(nome,cnpj);
-            cliente=pj;
-            pagamento=showInputDialog("Escolha o método de pagamento: \n"+
-                                        "V: pagamento à vista \n"+
-                                        "P: pagamento parcelado").toLowerCase();
-                if(pagamento.equals("v")){
-                    Reserva reserva1= new Reserva(cliente, true);
-                    reserva.add(reserva1);
-                }else if(pagamento.equals("p")){
-                    Reserva reserva1= new Reserva(cliente, false);
-                    reserva.add(reserva1);
-                }else{
-                    showMessageDialog(null, "Resposta inválida");
-                }
-        }else{
-            showMessageDialog(null, "Por favor, insira uma resposta válida");    
+            PessoaFisica pf = new PessoaFisica(nome, cpf);
+            cliente = pf;
+            pagamento = showInputDialog("Escolha o método de pagamento: \n" +
+                    "V: pagamento à vista \n" +
+                    "P: pagamento parcelado").toLowerCase();
+
+            if (pagamento.equals("v")) {
+                Reserva reserva1 = new Reserva(cliente, true);
+                reserva.add(reserva1);
+            } else if (pagamento.equals("p")) {
+                Reserva reserva1 = new Reserva(cliente, false);
+                reserva.add(reserva1);
+            } else {
+                showMessageDialog(null, "Resposta inválida");
+            }
+
+        } else if (tpPessoa.equals("j")) {
+            cnpj = showInputDialog("Insira seu CNPJ:");
+            PessoaJuridica pj = new PessoaJuridica(nome, cnpj);
+            cliente = pj;
+            pagamento = showInputDialog("Escolha o método de pagamento: \n" +
+                    "V: pagamento à vista \n" +
+                    "P: pagamento parcelado").toLowerCase();
+            if (pagamento.equals("v")) {
+                Reserva reserva1 = new Reserva(cliente, true);
+                reserva.add(reserva1);
+            } else if (pagamento.equals("p")) {
+                Reserva reserva1 = new Reserva(cliente, false);
+                reserva.add(reserva1);
+            } else {
+                showMessageDialog(null, "Resposta inválida");
+            }
+        } else {
+            showMessageDialog(null, "Por favor, insira uma resposta válida");
         }
 
-        if(reserva.size()<=6){
+        if (reserva.size() <= 6) {
             showMessageDialog(null, "Sua mesa foi reservada!");
-        }else{
+        } else {
             showMessageDialog(null, "Você está na lista de espera.");
         }
-        
+
     }
 
     public static void pesquisar() {
         String aux;
-        boolean achou=false;
+        boolean achou = false;
 
-        if(reserva.size()==0){
+        if (reserva.size() == 0) {
             showMessageDialog(null, "Ainda não há reservas");
-        }else{
-            aux=showInputDialog(null, "Insira seu CPF ou CNPJ:");
-            for(int i=0; i<reserva.size();i++){
-                if(reserva.get(i).getCliente() instanceof PessoaFisica){
-                    Cliente cliente= reserva.get(i).getCliente();
-                    PessoaFisica pf= (PessoaFisica) (cliente);
-                    if(pf.getCpf().equals(aux)){
-                        achou=true;
+        } else {
+            aux = showInputDialog(null, "Insira seu CPF ou CNPJ:");
+            for (int i = 0; i < reserva.size(); i++) {
+                if (reserva.get(i).getCliente() instanceof PessoaFisica) {
+                    Cliente cliente = reserva.get(i).getCliente();
+                    PessoaFisica pf = (PessoaFisica) (cliente);
+                    if (pf.getCpf().equals(aux)) {
+                        achou = true;
                         showMessageDialog(null, "Você possui reserva para o jantar");
                         break;
                     }
                 }
-                if(reserva.get(i).getCliente() instanceof PessoaJuridica){
-                    Cliente cliente= reserva.get(i).getCliente();
-                    PessoaJuridica pj= (PessoaJuridica) (cliente);
-                    if(pj.getCnpj().equals(aux)){
-                        achou=true;
+                if (reserva.get(i).getCliente() instanceof PessoaJuridica) {
+                    Cliente cliente = reserva.get(i).getCliente();
+                    PessoaJuridica pj = (PessoaJuridica) (cliente);
+                    if (pj.getCnpj().equals(aux)) {
+                        achou = true;
                         showMessageDialog(null, "Você possui reserva para o jantar");
                         break;
                     }
@@ -93,18 +92,27 @@ public class Util {
                 }
 
             }
-            if(achou==false){
+            if (achou == false) {
                 showMessageDialog(null, "Nenhuma reserva foi encontrada");
             }
 
         }
 
-
     }
 
     public static void imprimirReserva() {
-        
+        if (reserva.size() == 0) {
+            showMessageDialog(null, "Não há reservas");
+        } else {
+            for (int i = 0; i < reserva.size(); i++) {
+                if (i < 6) {
+                    reserva.get(i).getCliente().toString();
+                }
+            }
+        }
     }
+
+    
 
     public static void imprimirEspera() {
     }
@@ -112,6 +120,4 @@ public class Util {
     public static void cancelar() {
     }
 
-    
-    
 }
